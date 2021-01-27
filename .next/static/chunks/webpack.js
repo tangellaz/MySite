36160bed62d682cefd7669b6ad0f1d023ae9dab9
tangellaz <tangellaz@gmail.com>
2021-01-27 04:36:56 -0700

    media query test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1c845324de1ce98381e0";
+/******/ 	var hotCurrentHash = "d52d5f8c5d03db13b06d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
