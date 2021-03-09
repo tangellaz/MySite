55f0fac15fe1844787ebf074c23a61e6d468b35b
tangellaz <tangellaz@gmail.com>
2021-03-09 05:31:46 -0700

    resolving build error and removing dead page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7a98e4530040279dce47";
+/******/ 	var hotCurrentHash = "bd19c4089d6158350f9e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
